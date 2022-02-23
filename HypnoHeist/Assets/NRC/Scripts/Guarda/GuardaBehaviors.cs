ae1072f0823e8b602ef05d49b8017ee899d6a730
nrc <adao.nrc@edu.unifilbr>
2022-02-23 20:57:34 -0300

    delete Version Check, last version: 2020.3.29f1

--- HypnoHeist/Assets/NRC/Scripts/Guarda/GuardaBehaviors.cs
+++ HypnoHeist/Assets/NRC/Scripts/Guarda/GuardaBehaviors.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [System.Serializable]
 public class SyncPath {
+    [FormerlySerializedAs("inPointWaitFor")] public int pointToWaitFor;
     public GuardaBehaviors guarda;
-    public int mePoint;
-    public int youPoint;
+    public int guardaReachPoint;
 }
 
 public class GuardaBehaviors : MonoBehaviour {
@@ -27,7 +28,11 @@ public class GuardaBehaviors : MonoBehaviour {
     public List<SyncPath> syncPaths;
     private SyncPath waitingFor;
     
-    public bool ready;
+    bool reachedPoint;
+    private bool duringSync;
+    
+    // Detection
+    const int playerLayer = 1 << 3;
     
     void Awake() {
         control = GetComponent<Controller>();
@@ -45,10 +50,10 @@ public class GuardaBehaviors : MonoBehaviour {
             Physics2D.IgnoreCollision(guarda.torsoCol, torsoCol);
         }
 
-        transform.position =  patrolPoints[startPatrolIndex];
-        pathIndex = startPatrolIndex;
         
         if (patrolPoints.Count > 1) {
+            transform.position =  patrolPoints[startPatrolIndex];
+            pathIndex = startPatrolIndex;
             patroling = true;
             if (startPatrolIndex + 1 > patrolPoints.Count - 1) {
                 print("start tracing back");
@@ -56,28 +61,44 @@ public class GuardaBehaviors : MonoBehaviour {
             };
             CalculateNextPatrolPoint();
         }
+        
     }
 
     private void FixedUpdate() {
+        Vector2 dir = control.m_FacingRight ? transform.right : -transform.right ;
+        var hit = Physics2D.Raycast(transform.position, dir, 30, playerLayer);
+        if (hit) {
+            if (hit.collider.CompareTag("Player")) {
+                print( name + " sees player");
+            }
+        }
+        
         if (!patroling) return;
         Vector2 pos = transform.position;
         
         control.Move(Mathf.Sign(patrolTarget.x - pos.x ));
 
         if(Vector2.Distance(pos, patrolTarget) < 1) {
+            reachedPoint = true;
             if (CanCyclePatrol()) {
+                if(duringSync) return;
                 CalculateNextPatrolPoint();
+                
             }
         }        
     }
 
     private void CalculateNextPatrolPoint() {
+        if (patrolPoints.Count <= 1) return;
         pathIndex = inversePatrolling ? pathIndex - 1 : pathIndex + 1;
         patrolTarget = patrolPoints[pathIndex];
+        
+        if(duringSync) return;
+        reachedPoint = false;
     }
     
     private bool CanCyclePatrol() {
-        
+        if (patrolPoints.Count <= 1) return patroling =  false;
         
         if (pathIndex + 1 > patrolPoints.Count - 1) {
             inversePatrolling = true;            
@@ -87,27 +108,37 @@ public class GuardaBehaviors : MonoBehaviour {
 
 
         if (!(syncPaths.Count > 0)) return true;
-
-        foreach (var waitGuarda in syncPaths) {
-            if (waitGuarda.youPoint == pathIndex) {
-                waitingFor = waitGuarda;
-                StartCoroutine(AwaitGuardaReach());
-            }
+        
+        if (waitingFor != null) {
+            return waitingFor.guarda.reachedPoint;
+        }
+        
+        foreach (var syncPoint in syncPaths) {
             
+            if (syncPoint.pointToWaitFor == pathIndex) {
+                waitingFor = syncPoint;
+                StartCoroutine(AwaitGuardaReach());       
+            }
         }
         return true;
     }
 
 
     bool InPosition() {
-        return waitingFor.guarda.pathIndex == waitingFor.mePoint;
+        return waitingFor.guarda.pathIndex == waitingFor.guardaReachPoint && waitingFor.guarda.reachedPoint;
     }
 
     IEnumerator AwaitGuardaReach() {
-        print(name + "Stop patrolling");
+        duringSync = true;
+        yield return new WaitUntil(() => reachedPoint);
         patroling = false;
+        
         yield return new WaitUntil(() => InPosition());
+        yield return new WaitForSeconds(0.3f);
+        print(name +" sync with "+ waitingFor.guarda.name);
+        duringSync = false;
         patroling = true;
-        print(name + "Back patrolling");
+        waitingFor = null;
+        CalculateNextPatrolPoint();
     }
 }
