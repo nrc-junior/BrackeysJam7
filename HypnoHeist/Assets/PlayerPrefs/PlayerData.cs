140597e6046fdf17d581f91c833636607c1cae18
nrc <adao.nrc@edu.unifilbr>
2022-02-25 19:53:40 -0300

    added tutorial variables

---
+++ HypnoHeist/Assets/PlayerPrefs/PlayerData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour {
+    public static bool madeTutorial;
+    public static int defaultRng = 42;
+    public static int savedRng ;
+}
