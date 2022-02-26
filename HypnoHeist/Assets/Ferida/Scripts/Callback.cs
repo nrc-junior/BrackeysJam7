8a4c702dd7dc324918a37940c698a1c3f3a46f84
nrc <adao.nrc@edu.unifilbr>
2022-02-26 03:25:33 -0300

    changed Invoke when player interacted

--- HypnoHeist/Assets/Ferida/Scripts/Callback.cs
+++ HypnoHeist/Assets/Ferida/Scripts/Callback.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,16 +7,31 @@ public class Callback : MonoBehaviour
     [SerializeField] private UnityEvent triggerEventEnter; 
     [SerializeField] private UnityEvent triggerEventExit; 
     [SerializeField] private string objectTag = "Player";
+    [SerializeField] private bool interactive;
+
+    private PlayerBehaviours pb;
     private void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag(objectTag))
         {
+            if (pb == null) pb = other.GetComponent<PlayerBehaviours>();
+            if(interactive) return;
+            
             triggerEventEnter.Invoke();
         }   
     }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (pb == null || !interactive) return;
+        if(!pb.isInteracting && interactive) return;
+        pb.isInteracting = false;
+        triggerEventEnter.Invoke();
+    }
+
     private void OnTriggerExit2D(Collider2D other){
         if(other.gameObject.CompareTag(objectTag))
         {
             triggerEventExit.Invoke();
+            pb = null;
         }   
     }
 }
