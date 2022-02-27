52162441c882104fc4c7fca4959a6364f2a00a8d
nrc <adao.nrc@edu.unifilbr>
2022-02-27 03:33:16 -0300

    added more static variablesrData.

--- HypnoHeist/Assets/PlayerPrefs/PlayerData.cs
+++ HypnoHeist/Assets/PlayerPrefs/PlayerData.cs
@@ -7,6 +7,12 @@ public class PlayerData : MonoBehaviour {
     public static int defaultRng = 42;
     public static int savedRng ;
 
+    public static int playTimes;
+    static float[] botVelocity = {0.547f, 0.497f, 0.42f, .349f, 0.297f};
+
+    public static float GetBotVel() =>  
+        playTimes > botVelocity.Length - 1 ? botVelocity[botVelocity.Length - 1] : botVelocity[playTimes];
+    
     static public Collider2D[] playerCols;
     static public PlayerBehaviours pb;
 }
